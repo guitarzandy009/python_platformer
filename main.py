7d3489674c372d3b427fb34d1431d80b64f92ae2
Andy Stuckey <andystuckey@mail.com>
2025-11-24 11:52:40 -0500

    add header imports and some initial variables

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from python-platformer!")
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
-    main()
+    main(window)
